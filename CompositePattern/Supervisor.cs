72613bf2fbcdb31e48ebcecf2e6f5ddd4f60c2b8
trungpv88@gmail.com <trungpv88@gmail.com@859b276e-b915-b335-d686-3fcc8cd59103>
2014-09-22 10:32:12 +0000

    Composite Pattern

---
+++ CompositePattern/Supervisor.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CompositePattern
+{
+    public class Supervisor: IPeople
+    {
+        private readonly List<IPeople> m_lsStudent = new List<IPeople>();
+        public string Name { get; set; }
+
+        public void addStudent(IPeople employed)
+        {
+            m_lsStudent.Add(employed);
+        }
+
+        public void removeStudent(IPeople employed)
+        {
+            m_lsStudent.Remove(employed);
+        }
+
+        public IPeople getStudent(int index)
+        {
+            return m_lsStudent[index];
+        }
+
+        public void Render(int depth)
+        {
+            Console.WriteLine(new String('-', depth) + Name);
+            foreach (var student in m_lsStudent)
+            {
+                student.Render(depth + 1);
+            }
+        }
+    }
+}
