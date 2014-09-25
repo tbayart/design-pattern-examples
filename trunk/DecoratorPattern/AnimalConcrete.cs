41285149307f627bf33ef81f1b3bc6b51ebdd14e
trungpv88@gmail.com <trungpv88@gmail.com@859b276e-b915-b335-d686-3fcc8cd59103>
2014-09-25 09:05:47 +0000

    Decorator Pattern

---
+++ trunk/DecoratorPattern/AnimalConcrete.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DecoratorPattern
+{
+    public class AnimalConcrete : AnimalBase
+    {
+        public override void operation()
+        {
+            Console.WriteLine("- I'm a animal and i can eat.");
+        }
+    }
+}
---
+++ trunk/DecoratorPattern/PeopleConcrete.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DecoratorPattern
+{
+    public class PeopleConcrete : PeopleDecorator
+    {
+        public PeopleConcrete(AnimalBase animalBase)
+            : base(animalBase)
+        {
+
+        }
+
+        public override void operation()
+        {
+            base.operation();
+            Console.WriteLine("+ I'm a person and i can  think!");
+        }
+    }
+}
