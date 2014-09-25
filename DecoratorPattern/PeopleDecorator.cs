997843fe70021ecd9b7ca77dd657cafec8b76b80
trungpv88@gmail.com <trungpv88@gmail.com@859b276e-b915-b335-d686-3fcc8cd59103>
2014-09-25 09:05:47 +0000

    Decorator Pattern

---
+++ DecoratorPattern/PeopleDecorator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DecoratorPattern
+{
+    public class PeopleDecorator : AnimalBase
+    {
+        private readonly AnimalBase m_animal;
+
+        public PeopleDecorator(AnimalBase animal)
+        {
+            m_animal = animal;
+        }
+
+        public override void operation()
+        {
+            m_animal.operation();
+        }
+    }
+}
---
+++ DecoratorPattern/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DecoratorPattern
+{
+    /// <summary>
+    /// Extend the functionality of individual object. Here:
+    /// - animalConcrete: individual object
+    /// - peopleConcrete: extended object
+    /// Ex: applyAlgorithm() in Lena
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var animalConcrete = new AnimalConcrete();
+            var peopleConcrete = new PeopleConcrete(animalConcrete);
+            peopleConcrete.operation();
+
+            Console.ReadKey();
+        }
+    }
+}
