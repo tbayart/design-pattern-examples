30559762b8d6e45ce97762ea69be66987126e793
trungpv88@gmail.com <trungpv88@gmail.com@859b276e-b915-b335-d686-3fcc8cd59103>
2014-09-29 02:22:31 +0000

    Facade Pattern

---
+++ trunk/FacadePattern/CPU.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FacadePattern
+{
+    public class CPU
+    {
+        public CPU()
+        {
+        }
+
+        public void Freeze()
+        {
+            Console.WriteLine("CPU is frozen.");
+        }
+
+        public void Excute()
+        {
+            Console.WriteLine("CPU has been excuted.");
+        }
+
+        public void Jump(long position)
+        {
+            Console.WriteLine("CPU jump into {0}", position);
+        }
+    }
+}
---
+++ trunk/FacadePattern/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FacadePattern
+{
+    /// <summary>
+    /// To synthesis subclasses and subsystems
+    /// To provide an interface to acess subsystems and subclasses
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var computer = new Computer();
+            computer.Run();
+            Console.ReadKey();
+        }
+    }
+}
