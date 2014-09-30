52fdfcade6632db40b743c5d571fdaa56d786691
trungpv88@gmail.com <trungpv88@gmail.com@859b276e-b915-b335-d686-3fcc8cd59103>
2014-09-30 02:11:49 +0000

    Proxy Pattern

---
+++ ProxyPattern/Player.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProxyPattern
+{
+    public abstract class Player
+    {
+        public abstract void Shoot();
+        public abstract void Hand();
+    }
+}
