19a313a48ce1f2a1646b52114a1c5a0156e80494
trungpv88@gmail.com <trungpv88@gmail.com@859b276e-b915-b335-d686-3fcc8cd59103>
2014-10-04 10:48:34 +0000

    Interpreter Pattern

---
+++ InterpreterPattern/DoubleExpression.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace InterpreterPattern
+{
+    public class DoubleExpression: IExpressionBase
+    {
+        double m_value;
+
+        public DoubleExpression(double value)
+        {
+            m_value = value;
+        }
+
+        public double Evaluate()
+        {
+            return m_value;
+        }
+
+        public override string ToString()
+        {
+            return m_value.ToString();
+        }
+    }
+}
---
+++ InterpreterPattern/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace InterpreterPattern
+{
+    /// <summary>
+    /// Lena software: evaluate expression in LUT-Enhancement
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var parser = new Parser();
+            IExpressionBase expression = parser.Parse("+ 5 6");
+            Console.WriteLine("{0} = {1}", expression, expression.Evaluate());
+            Console.ReadKey();
+        }
+    }
+}
