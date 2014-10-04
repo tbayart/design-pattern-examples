12fd770e8284f60085bbd46f682884698a957d07
trungpv88@gmail.com <trungpv88@gmail.com@859b276e-b915-b335-d686-3fcc8cd59103>
2014-10-04 10:48:34 +0000

    Interpreter Pattern

---
+++ trunk/InterpreterPattern/AddExpression.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace InterpreterPattern
+{
+    public class AddExpression: IExpressionBase
+    {
+        IExpressionBase m_expr1;
+        IExpressionBase m_expr2;
+        public AddExpression(IExpressionBase expr1, IExpressionBase expr2)
+        {
+            m_expr1 = expr1;
+            m_expr2 = expr2;
+        }
+
+        public double Evaluate()
+        {
+            double value1 = m_expr1.Evaluate();
+            double value2 = m_expr2.Evaluate();
+            return value1 + value2;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0} + {1}", m_expr1, m_expr2);
+        }
+    }
+}
---
+++ trunk/InterpreterPattern/Parser.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace InterpreterPattern
+{
+    public class Parser
+    {
+        public IExpressionBase Parse(string polish)
+        {
+            var symbols = new List<string>(polish.Split(' '));
+            return ParseNextExpression(symbols);
+        }
+
+        public IExpressionBase ParseNextExpression(List<string> symbols)
+        {
+            int value;
+            if (int.TryParse(symbols[0], out value))
+            {
+                symbols.RemoveAt(0);
+                return new DoubleExpression(value);
+            }
+            return ParseNonTerminalExpression(symbols);
+        }
+
+        private IExpressionBase ParseNonTerminalExpression(List<string> symbols)
+        {
+            var symbol = symbols[0];
+            symbols.RemoveAt(0);
+
+            var expr1 = ParseNextExpression(symbols);
+            var expr2 = ParseNextExpression(symbols);
+
+            switch (symbol)
+            {
+                case "+":
+                    return new AddExpression(expr1, expr2);
+                default:
+                    string message = string.Format("Invalid Symbol ({0})", symbol);
+                    throw new InvalidOperationException(message);
+            }
+        }
+    }
+}
