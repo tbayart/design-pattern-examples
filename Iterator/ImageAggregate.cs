06cd40a6b15f48d02393c0a6191ce6b5cff01fbb
trungpv88@gmail.com <trungpv88@gmail.com@859b276e-b915-b335-d686-3fcc8cd59103>
2014-10-06 10:40:21 +0000

    Iterator Pattern

---
+++ Iterator/ImageAggregate.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IteratorPattern
+{
+    public class ImageAggregate: IImageEnumerable
+    {
+        private List<ImageBase> m_images = new List<ImageBase>();
+
+        public ImageBase this[int index]
+        {
+            get { return m_images[index]; }
+        }
+
+        public IImageIterator GetIterator()
+        {
+            return new ImageEnumerator(this);
+        }
+
+        public int Count
+        {
+            get { return m_images.Count;}
+        }
+
+        public void Add(ImageBase image)
+        {
+            m_images.Add(image);
+        }
+    }
+}
