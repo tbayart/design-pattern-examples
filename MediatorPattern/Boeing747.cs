d9e4387bbe699a6a603eabc473a186b464ed367e
trungpv88@gmail.com <trungpv88@gmail.com@859b276e-b915-b335-d686-3fcc8cd59103>
2014-10-07 10:39:49 +0000

    Mediator Pattern

---
+++ MediatorPattern/Boeing747.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MediatorPattern
+{
+    public class Boeing747: Aircraft
+    {
+        public Boeing747(string registrationNumber, IAirTrafficControl act)
+            : base(registrationNumber, act)
+        {
+        }
+    }
+}
---
+++ MediatorPattern/IAirTrafficControl.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MediatorPattern
+{
+    public interface IAirTrafficControl
+    {
+        void RegistrerAircraft(Aircraft aircraft);
+        void SendWarningMessage(Aircraft aircraft);
+    }
+}
