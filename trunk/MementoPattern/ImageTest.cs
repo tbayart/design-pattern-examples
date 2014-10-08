1dee8ff04a4aba21dbff8b75b77b6a40728dc132
trungpv88@gmail.com <trungpv88@gmail.com@859b276e-b915-b335-d686-3fcc8cd59103>
2014-10-08 07:48:47 +0000

    Memento Pattern

---
+++ trunk/MementoPattern/ImageTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MementoPattern
+{
+    public class ImageTest
+    {
+        public string Name { get; set; }
+        public bool Status { get; set; }
+
+        public TestMemento CreateMemento()
+        {
+            return new TestMemento(Name, Status);
+        }
+
+        public void Undo(TestMemento testMemento)
+        {
+            Name = testMemento.Name;
+            Status = testMemento.Status;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine("Image Name: {0}, Status: {1}", Name, Status);
+        }
+    }
+}
---
+++ trunk/MementoPattern/TestCareTaker.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MementoPattern
+{
+    public class TestCareTaker
+    {
+        readonly Stack<TestMemento> m_testMemento = new Stack<TestMemento>();
+
+        public TestMemento GetMemento()
+        {
+            return m_testMemento.Pop();
+        }
+
+        public void Add(TestMemento testMemento)
+        {
+            m_testMemento.Push(testMemento);
+        }
+    }
+}
