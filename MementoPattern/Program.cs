9c16801ce7bf25a52b86a5922889d45531d16ae0
trungpv88@gmail.com <trungpv88@gmail.com@859b276e-b915-b335-d686-3fcc8cd59103>
2014-10-08 07:48:47 +0000

    Memento Pattern

---
+++ MementoPattern/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MementoPattern
+{
+    /// <summary>
+    /// Save current state of object without breaking the rules of encapsulation
+    /// Ex: save test history
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var testCareTaker = new TestCareTaker();
+            var imageTest = new ImageTest { Name = "Image1.bmp", Status = true};
+            testCareTaker.Add(imageTest.CreateMemento());
+            imageTest = new ImageTest { Name = "Image2.bmp", Status = false };
+            testCareTaker.Add(imageTest.CreateMemento());
+            imageTest = new ImageTest { Name = "Image3.bmp", Status = true };
+            imageTest.ShowInfo();
+
+            imageTest.Undo(testCareTaker.GetMemento());
+            imageTest.ShowInfo();
+            imageTest.Undo(testCareTaker.GetMemento());
+            imageTest.ShowInfo();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MementoPattern/TestMemento.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MementoPattern
+{
+    public class TestMemento
+    {
+        public string Name { get; private set; }
+        public bool Status { get; private set; }
+        
+        public TestMemento(string name, bool status)
+        {
+            Name = name;
+            Status = status;
+        }
+    }
+}
