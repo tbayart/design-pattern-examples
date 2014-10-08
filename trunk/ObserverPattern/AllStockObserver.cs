3ddcb876f59b39b25c65cbe695230f18d27d499e
trungpv88@gmail.com <trungpv88@gmail.com@859b276e-b915-b335-d686-3fcc8cd59103>
2014-10-08 10:30:13 +0000

    Observer Pattern

---
+++ trunk/ObserverPattern/AllStockObserver.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ObserverPattern
+{
+    public class AllStockObserver: IStockObserverBase
+    {
+        public string Name { get; set;}
+        
+        public AllStockObserver(string name)
+        {
+            Name = name;
+        }
+
+        public void Notify(Stock stock)
+        {
+            // Print value changed
+            Console.WriteLine("Notified {0} of {1}'s " +
+                          "change to {2:C}", Name, stock.Code, stock.Price);
+        }
+    }
+}
---
+++ trunk/ObserverPattern/Stock.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ObserverPattern
+{
+    public class Stock
+    {
+        public decimal Price { get; set; }
+        public string Code { get; set; }
+    }
+}
