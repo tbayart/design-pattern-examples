376b9745040c9ed4956376ee30fac7f1c75fab3c
trungpv88@gmail.com <trungpv88@gmail.com@859b276e-b915-b335-d686-3fcc8cd59103>
2014-10-09 04:39:59 +0000

    State Pattern

---
+++ trunk/StatePattern/ButtonBase.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StatePattern
+{
+    public class ButtonBase
+    {
+        private ButtonState m_state;
+
+        public ButtonState State
+        {
+            get { return m_state; }
+            set { m_state = value; }
+        }
+         
+        public ButtonBase()
+        {
+            m_state = new PauseState();
+        }
+
+        public void PressPlayButton()
+        {
+            m_state.PlayButtonPressed(this);
+        }
+
+        public void PressPauseButton()
+        {
+            m_state.PauseButtonPressed(this);
+        }
+    }
+}
