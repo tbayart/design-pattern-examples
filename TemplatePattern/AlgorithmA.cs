384d2b6086133d07c3e07e77299537fa595e8855
trungpv88@gmail.com <trungpv88@gmail.com@859b276e-b915-b335-d686-3fcc8cd59103>
2014-10-09 09:54:44 +0000

    Template Pattern

---
+++ TemplatePattern/AlgorithmA.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TemplatePattern
+{
+    public class AlgorithmA: AlgoithmBase
+    {
+        public override void Step1()
+        {
+            Console.WriteLine("Algo A -> Step1");
+        }
+
+        public override void Step2()
+        {
+            Console.WriteLine("Algo A -> Step2");
+        }
+    }
+}
---
+++ TemplatePattern/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TemplatePattern
+{
+    class Program
+    {
+        /// <summary>
+        /// The template method pattern is a design pattern that allows a group of interchangeable, 
+        /// similarly structured, multi-step algorithms to be defined. 
+        /// Each algorithm follows the same series of actions but provides a different implementation 
+        /// of the steps.
+        /// ~ Strategy Pattern
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            AlgoithmBase algoA = new AlgorithmA();
+            algoA.TemplateMethod();
+            AlgoithmBase algoB = new AlgorithmB();
+            algoB.TemplateMethod();
+            Console.ReadKey();
+        }
+    }
+}
