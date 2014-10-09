aa7e0661083785419cdcbfb8334e5f6f3ec6ae79
trungpv88@gmail.com <trungpv88@gmail.com@859b276e-b915-b335-d686-3fcc8cd59103>
2014-10-09 10:29:04 +0000

    Visitor Pattern

--- trunk/DesignPattern/Program.cs
+++ trunk/DesignPattern/Program.cs
@@ -7,6 +7,13 @@ namespace DesignPattern
 {
     class Program
     {
+        /// <summary>
+        /// Thes design patterns were implemented base on the articles of Kanasz Robert on codeproject.com:
+        /// http://www.codeproject.com/Articles/430590/Design-Patterns-of-Creational-Design-Patterns
+        /// http://www.codeproject.com/Articles/438922/Design-Patterns-of-Structural-Design-Patterns
+        /// http://www.codeproject.com/Articles/455228/Design-Patterns-of-Behavioral-Design-Patterns
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
         }
