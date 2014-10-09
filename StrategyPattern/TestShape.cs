f4437b7c3d9a9ea693fa406714dbbe6db69191e8
trungpv88@gmail.com <trungpv88@gmail.com@859b276e-b915-b335-d686-3fcc8cd59103>
2014-10-09 09:28:05 +0000

    Strategy Pattern

---
+++ StrategyPattern/TestShape.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StrategyPattern
+{
+    public class TestShape: TestBase
+    {
+        private int m_randomNumber;
+        public TestShape()
+        {
+            Random rnd = new Random();
+            m_randomNumber = rnd.Next(0, 10);
+            System.Threading.Thread.Sleep(100);
+        }
+
+        public override bool isGood()
+        {
+            return m_randomNumber > 5;
+        }
+
+        public override bool isNG()
+        {
+            return !isGood();
+        }
+    }
+}
